d2dce82a5ae5c9537dc98e84f261faf7423e57e7
simgyuryeol <66519915+simgyuryeol@users.noreply.github.com>
2021-03-07 20:40:25 +0900

    백준 11047번 해결

--- TheSim/11047_python.py
+++ TheSim/11047_python.py
@@ -1,1 +1,23 @@
-print("hellow world")
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+n,k=input().split()
+n=int(n)
+k=int(k)
+
+count=0
+
+money=[]
+for i in range(n):
+    money.append(int(input()))
+
+for i in range(n,0,-1):
+    if money[i-1]>k:
+        continue
+    else:
+        count += k//money[i-1]
+        k = k%money[i-1]
+
+print(count)
+    
+
