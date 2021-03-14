3c3a4bdd19b6a4f44d80693b06dfcfe93722bc71
KIMTHE <kcms2369@naver.com>
2021-03-14 18:36:56 +0900

    readme

--- KIMTHE_ps/tmp.py
+++ KIMTHE_ps/tmp.py
@@ -5,28 +5,38 @@ from itertools import combinations, product
 input = lambda : sys.stdin.readline().rstrip()
 
 n = int(input())
-nums = [int(input()) for _ in range(n)]
 
-inter = []
+nums = [True] * (n+1)
 
-for i in range(1,n) : inter.append(nums[i]-nums[i-1])
+for i in range(2,int(math.sqrt(n))+1):
+    if nums[i] == False : continue
 
-def uclid(a,b):
-    if a>b: a,b = b,a
-    tmp = b%a
+    tmp = i*2
+    while tmp<=n :
+        nums[tmp] = False
+        tmp += i
 
-    if tmp == 0: return a
+pri = []
 
-    else : return(uclid(tmp,a))
-    
-tmp = inter[0]
-for i in inter:
-    tmp = uclid(tmp,i)
+for i in range(2,n+1):
+    if nums[i] == True:
+        pri.append(i)
+
 
 result = 0
 
-for i in inter:
-    result += (i // tmp - 1)
+for i in range(len(pri)):
+    tmp = 0
 
+    for idx in range(i,len(pri)) :
+        tmp += pri[idx]
+
+        if tmp == n : 
+            result += 1
+            break
+        elif tmp > n :
+            break
+
+        
 
 print(result)
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # problem-solve
+
 ### `백준에서 풀이한 알고리즘 문제코드`
-- 풀이하는데 시간이 소비되었다면 기록한다.
+- 풀이하는데 어느정도 시간이 소비되었다면 기록한다.
 - language : python
-- 1 day, 2 solve
+- 1 day, 1-2 solve!
 - 1 month, 2 level up!
 
  ___
@@ -20,10 +21,11 @@
 
 ___
 
-### **<기타 팁>**
+## **<기타 팁>**
  
 
-<최대공약수>
+### 최대공약수)
+
 
 python의 유클리드 호제법에 해당하는 함수가 존재한다.
 
@@ -31,6 +33,22 @@ python의 유클리드 호제법에 해당하는 함수가 존재한다.
     import math
     math.gcd(a,b)
 
+### 소수판별)
 
+
+에라토스테네스의 체 알고리즘을 이용하여 1부터 n까지의 소수를 판별한다.
+
+    import math
+
+    array = [True] * (n+1)
+    array[1] = False # 1은 소수가 아님
+
+    for i in range(2,int(math.sqrt(n))+1) :
+        if array[i] == False : continue
+
+        tmp = i*2
+        while tmp <= n :
+            array[tmp] = False # 소수의 배수는 소수가 아님
+            tmp += i
 
  
