572e6cab138c338a1f1c2a2be8a80cd331f80862
simgyuryeol <66519915+simgyuryeol@users.noreply.github.com>
2021-05-12 23:36:33 +0900

    6588_python 소수판별반복문으로 해결 조건식 잘 따져보기

--- TheSim_ps/sex/6588_python.py
+++ TheSim_ps/sex/6588_python.py
@@ -5,7 +5,6 @@ input = lambda : sys.stdin.readline().rstrip()
 
 while True:
     wrong=0 # 1이면 반례
-
     N=int(input())
     if N==0:
         break
@@ -15,8 +14,13 @@ while True:
         if i%2==0:
             continue
         prime=0 # 소수인지 아닌지 확인
-
+           
         sosu=N-i
+        for j in range(2,int(i**0.5)+1):
+            if i%j==0:
+                prime=1
+                break
+
 
         for j in range(2,int(sosu**0.5)+1):
             if sosu%j==0:
