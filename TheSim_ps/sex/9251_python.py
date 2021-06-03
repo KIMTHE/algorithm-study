24256d291f24b22d2d9bd12b55562a075aa0a948
simgyuryeol <66519915+simgyuryeol@users.noreply.github.com>
2021-06-03 20:09:56 +0900

    9251_python LCS알고리즘으로 해결

--- TheSim_ps/sex/9251_python.py
+++ TheSim_ps/sex/9251_python.py
@@ -1,3 +1,25 @@
 import sys
 input = lambda : sys.stdin.readline().rstrip()
 
+A=input()
+B=input()
+
+A=list(A)
+B=list(B)
+
+output=[[0]*(len(A)+1)for i in range(len(B)+1)]
+
+
+for i in range(len(A)):
+    for j in range(len(B)):
+        if A[i]==B[j]:
+            output[j+1][i+1]=output[j][i]+1
+        else:
+            output[j+1][i+1]=max(output[j][i+1],output[j+1][i])
+
+print(output[-1][-1])
+
+#for i in range(len(A)+1):
+ #   for j in range(len(B)+1):
+  #     print(output[j][i],end=' ')
+   # print()
