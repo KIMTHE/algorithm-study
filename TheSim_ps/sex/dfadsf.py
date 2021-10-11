0b3fdf46a264747290203c6b31bb249ec721a956
simgyuryeol <66519915+simgyuryeol@users.noreply.github.com>
2021-10-11 18:02:34 +0900

    7576_python bfs와 뎁큐를 사용하여 해결

--- TheSim_ps/sex/dfadsf.py
+++ TheSim_ps/sex/dfadsf.py
@@ -1,24 +1,33 @@
-import math
-
-def solution():
-    MIN, MAX = map(int, input().split(' '))
-
-    num = [True] * (MAX - MIN + 1)
-    count = 0
-    N = 1
-
-    while N * N  <= MAX:
-        N += 1
-        square = N * N
-        i = MIN // square
-
-        while square * i <= MAX:
-            idx = square * i - MIN
-
-            if idx >= 0 and num[idx]:
-                count += 1
-                num[idx] = False
-            i += 1
-    print(len(num) - count)
-
-solution()
+import sys
+from collections import deque
+input = sys.stdin.readline
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+def bfs():
+    q = deque()
+    q.append([0, 0, 1])
+    visit = [[[0] * 2 for i in range(m)] for i in range(n)]
+    print(visit)
+    visit[0][0][1] = 1
+    while q:
+        a, b, w = q.popleft()
+        if a == n - 1 and b == m - 1:
+            print(visit)
+            return visit[a][b][w]
+        for i in range(4):
+            x = a + dx[i]
+            y = b + dy[i]
+            if 0 <= x < n and 0 <= y < m:
+                if s[x][y] == 1 and w == 1:
+                    visit[x][y][0] = visit[a][b][1] + 1
+                    q.append([x, y, 0])
+                elif s[x][y] == 0 and visit[x][y][w] == 0:
+                    visit[x][y][w] = visit[a][b][w] + 1
+                    q.append([x, y, w])
+    print(visit)
+    return -1
+n, m = map(int, input().split())
+s = []
+for i in range(n):
+    s.append(list(map(int, list(input().strip()))))
+print(bfs())
