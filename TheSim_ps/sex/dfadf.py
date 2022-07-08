131fa56578d4a2c7a4374c62ce26d7781cd62ddf
simgyuryeol <66519915+simgyuryeol@users.noreply.github.com>
2022-07-08 19:22:17 +0900

    2565_python DP,LIS를 이용하여 해결

--- TheSim_ps/sex/dfadf.py
+++ TheSim_ps/sex/dfadf.py
@@ -1,50 +1,25 @@
-def find(card1, card2): 
-    answer=0
+#LIS를 유지하기 위해 숫자가 들어갈 위치를 이분탐색으로 찾는 함수
+def binarysearch(left, right, target):
+	#LIS 배열에 들어갈 target=arr[i]의 위치를 이분탐색으로 찾기
+    while left<right:
+        mid=(left+right)//2
+		
+        if lis[mid]<target:
+            left=mid+1
+        else:
+            right=mid
+    return right
 
-    user1=[] #받은 카드 저장
-    user2=[]     
 
-    for i,j in zip(card1,card2):
-        # 두 플레이어 카드 비교
-        # for a in i:
-        #     if a in j: #카드가 있다면
-        #         answer+=1
-        #         break
-        value= i+j
-        for a in value:
-            if value.count(a)>=2:
-                answer+=1
-                break
+arr= [2,1,3,5,4]
+lis= [] 
+lis.append(arr[0])
 
-        else: #직전에 받은 카드 비교
-            visit=0 #중복 여부
-            if user1:
-                for a in i:
-                    if a in user1[-1]:
-                        visit+=1
-                    if visit==2:
-                        answer+=1
-                        break
-                else:
-                    visit=0 #중복 여부
-                    for a in j:
-                        if a in user2[-1]:
-                            visit+=1
-                        if visit==2:
-                            answer+=1
-                            break
-        user1.append(i)
-        user2.append(j)
-
-    print(answer)
-                    
-            
-
-    return answer
-
-#한 라운드에서 두 플레이어가 받은 카드 10장중 중복
-# 한 플레이어 직전에 받은 카드 5장과 이번에 2개 이상 중복
-
-card1 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10],[13, 21, 24, 29, 50], [1, 12, 20, 21, 32], [16, 26, 34, 46, 52], [9, 11, 16, 16, 21], [3, 8, 10, 16, 20]]
-card2 = [[5, 7, 9, 11, 13], [11, 13, 15, 17, 19],[5, 10, 15, 41, 49], [6, 14, 15, 19, 46], [5, 42, 43, 51, 52], [5, 6, 11, 13, 45], [5, 9, 11, 13, 45]]
-find(card1,card2) 
+for value in arr:
+    if lis[-1]<value: 
+        lis.append(value)
+    else:
+        idx=binarysearch(0,len(lis)-1,value)
+        lis[idx]=value
+    
+print(len(lis))
