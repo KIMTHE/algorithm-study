34c9efde946279b85a0862cdaef6855919fcdde4
simgyuryeol <66519915+simgyuryeol@users.noreply.github.com>
2022-07-20 10:42:00 +0900

    1904_python dp로 해결

--- TheSim_ps/sex/dfadf.py
+++ TheSim_ps/sex/dfadf.py
@@ -1,6 +1,17 @@
-a = 100
-result = 0
-for i in range(1,3):
-   result = a >> i
-   result = result + 1
-print(result)
+import sys
+# sys.stdin=open('input.txt')
+from collections import deque
+mod = 10**9
+
+N=int(sys.stdin.readline())
+mm=[[0]*10 for _ in range(N+2)]
+mm[1]=[0,1,1,1,1,1,1,1,1,1]
+
+for i in range(2,N+1):
+    mm[i][0] = mm[i-1][1]
+    mm[i][9] = mm[i-1][8]
+    for j in range(1,9):
+        mm[i][j] = mm[i-1][j-1]+mm[i-1][j+1]
+
+# print(mm)
+print(sum(mm[N])%mod)
