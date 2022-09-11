4b1aeaaa65f806e04d5f7b57c52954ac36b2c689
simgyuryeol <66519915+simgyuryeol@users.noreply.github.com>
2022-09-11 17:59:28 +0900

    9370_python 다익스트라로 해결

--- TheSim_ps/python/9370_python.py
+++ TheSim_ps/python/9370_python.py
@@ -14,8 +14,6 @@ for _ in range(T):
 
     for _ in range(m):
         a,b,d = map(int,input().split())
-        if (a==g and b==h) or (a==h and b==g):
-            d-=0.1
         road[a].append([b,d])
         road[b].append([a,d])
     
@@ -39,14 +37,23 @@ for _ in range(T):
         for i,j in (road[node]): #도착 노드, 도착노드까지 비용
                 weighted_dis = cureent + j
 
-                if weighted_dis < distances[i] and node!=i:
+                if weighted_dis <= distances[i] and node!=i:
+                    if (node==g and i==h) or (node==h and i==g) or visit[node]:
+                        visit[i]= True
+                    elif weighted_dis == distances[i]:
+                        if visit[i]:
+                            visit[i]=True
+                        continue
+                    elif visit[node]==False:
+                        visit[i]=False
+
+
                     distances[i]=weighted_dis
                     if (weighted_dis,i) not in q:
                         heapq.heappush(q,(weighted_dis,i))
-
+        
     dest.sort()
-    for i in range(len(distances)):
-        if (distances[i]-int(distances[i]))!=0:
-            if i in dest:
-                print(i,end=" ")
+    for k in dest:
+        if visit[k]:
+            print(k,end=" ")
     print()
