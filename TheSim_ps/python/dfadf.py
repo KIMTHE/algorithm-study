5a2e5d9b646c39fc6285b10836bd7a0200bb0356
simgyuryeol <66519915+simgyuryeol@users.noreply.github.com>
2022-11-09 21:03:48 +0900

    프로그래머스 경주로 건설 bfs+dp로 해결

--- TheSim_ps/python/dfadf.py
+++ TheSim_ps/python/dfadf.py
@@ -12,36 +12,36 @@ def solution(board):
     dir=[[0,1],[0,-1],[1,0],[-1,0]]
     while q:
         x,y,t_value,p=q.popleft() #t는 타입 1은 상하, 2는 좌우
+
+        if x==n-1 and y==n-1:
+            continue
+
         for a,b in dir:
             xa=x+a
             yb=y+b
             
             if 0<=xa<n and 0<=yb<n and board[xa][yb]==0:
                 if t_value==0 or (t_value==1 and b==0) or (t_value==2 and a==0) : #직선, t==0일 경우는 첫 시작으로 전부다 직선     
-                    
-                    if visit[xa][yb]>=p+100:
                         if t_value==0 and b==0:
                             t=1
                         elif t_value==0 and a==0:
                             t=2
                         else:
                             t=t_value
-                        q.append([xa,yb,t,p+100])
-                        visit[xa][yb]=p+100
+                        np=p+100
                         
                 elif (t_value==1 and a==0) or (t_value==2 and b==0): #코너
-                    if visit[xa][yb]>=p+600:
                         if t_value==1:
                             t=2
                         else:
                             t=1
-                        q.append([xa,yb,t,p+600])
-                        visit[xa][yb]=p+600
+                        np=p+600
+                
+                if np<=visit[xa][yb]:
+                    visit[xa][yb]=np
+                    q.append([xa,yb,t,np])
     
-
-                        
-    answer=visit[-1][-1]
-    #answer=min(visit[0][-1][-1],visit[1][-1][-1])
+    print(visit)
     return answer
 
 solution([[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 0, 1, 0, 0], [1, 0, 0, 0, 1], [0, 1, 1, 0, 0]])
